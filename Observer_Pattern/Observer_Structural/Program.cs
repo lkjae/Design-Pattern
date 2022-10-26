f4ac7882529740c91a7de2cb85fe65524095e350
Lee KJ <112466589+lkjae@users.noreply.github.com>
2022-10-26 16:28:54 +0900

    Add files via upload

---
+++ Observer_Pattern/Observer_Structural/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer.Structural
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Configure Observer pattern
+            ConcreteSubject s = new ConcreteSubject();
+
+            s.Attach(new ConcreteObserver(s, "X"));
+            s.Attach(new ConcreteObserver(s, "Y"));
+            s.Attach(new ConcreteObserver(s, "Z"));
+
+            //Change subject and notify observers
+            s.SubjectState = "ABC";
+            s.Notify();
+
+            //Wait for user
+            Console.ReadKey();
+
+        }
+    }
+
+    public abstract class Subject
+    {
+        private List<Observer> observers = new List<Observer>();
+
+        public void Attach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(Observer observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Notify()
+        {
+            foreach(Observer o in observers)
+            {
+                o.Update();
+            }
+        }
+    }
+    public class ConcreteSubject : Subject
+    {
+        private string subjectState;
+
+        public string SubjectState
+        {
+            get { return subjectState; }
+            set { subjectState = value; }
+        }
+    }
+    public abstract class Observer
+    {
+        public abstract void Update();
+    }
+    
+    public class ConcreteObserver : Observer
+    {
+        private string name;
+        private string observerState;
+        private ConcreteSubject subject;
+
+        //Constructor
+        public ConcreteObserver(ConcreteSubject subject, string name)
+        {
+            this.subject = subject;
+            this.name = name;
+
+        }
+
+        public override void Update()
+        {
+            observerState = subject.SubjectState;
+            Console.WriteLine("Observer {0}'s new state is {1}", name, observerState);
+        }
+
+        public ConcreteSubject Subject
+        {
+            get { return subject; }
+            set { subject = value; }
+        }
+    }
+}
